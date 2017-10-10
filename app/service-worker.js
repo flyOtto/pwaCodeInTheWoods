91b611b817a55f6c1fbd401415b687101eea7191
laurisvan <lauri.svan@iki.fi>
2017-10-10 16:43:50 +0300

    Update service worker into a working example

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,27 +1,48 @@
 'use strict';
 
 // This file is a stub where Workbox will inject the results of Gulp build process
-// Source modified from https://github.com/ampproject/amp-publisher-sample/blob/master/amp-pwa-reader/src/sw.js
 importScripts('/scripts/service-worker/workbox.js');
 
-const workboxSW = new WorkboxSW({
-  clientsClaim: true,
-  skipWaiting: true
+const workboxSW = new WorkboxSW();
+
+// Static precaching list, injected by the build
+workboxSW.precache([
+]);
+
+// Workbox can intercept pushState changes and serve a static file, instead
+// workboxSW.router.registerNavigationRoute(
+//   'index.html', {
+//     whitelist: [/./],
+//     blacklist: [/images\/.*/, /\.(js|css)/],
+//   }
+// );
+
+// Permit same domain resources to be cached locally, but refresh from network
+// Permit everything except browser-sync
+workboxSW.router.registerRoute(
+  '/(.*)',
+  workboxSW.strategies.networkFirst()
+);
+
+// Caching of external contents fails by default, hence create a new rule
+const cacheOneWeekStrategy = workboxSW.strategies.cacheFirst({
+  cacheName: 'cdn-cache',
+  cacheExpiration: {
+    maxEntries: 20,
+    maxAgeSeconds: 7 * 24 * 60 * 60, // one week
+  },
+  cacheableResponse: {
+    statuses: [0, 200],
+  },
 });
 
-// Static precaching of images
-workboxSW.precache([]);
+// Cache external fonts, including the fonts from Google CDN
+workboxSW.router.registerRoute(
+  'https://fonts.googleapis.com/(.*)',
+  cacheOneWeekStrategy
+);
 
-// Register main route for all navigation links to pages
-workboxSW.router.registerNavigationRoute('index.html', {
-  whitelist: [/./],
-  blacklist: [/images\/.*/, /\.(js|css)/]
-});
-
-// Cache external libraries and fonts
-workboxSW.router.registerRoute('https:/fonts.googleapis.com/(.*)', workboxSW.strategies.cacheFirst());
-
-// Make sure new versions of the Service Worker activate immediately
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
+workboxSW.router.registerRoute(
+  'https://fonts.gstatic.com/(.*)',
+  cacheOneWeekStrategy
+);
